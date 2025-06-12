6995aebe2adc8bd3e0f5583750bf71d0173b8454
tkdgolden <93563260+tkdgolden@users.noreply.github.com>
2025-06-12 18:57:34 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -2,16 +2,31 @@ function spin() {
     if (document.getElementById("positionLocked").checked != true) {
         const select = document.getElementById("position");
         const randomIndex = Math.floor(Math.random() * select.options.length);
-        select.value = randomIndex;
+        if (select.value == randomIndex) {
+            select.value = randomIndex ++;
+        }
+        else {
+            select.value = randomIndex;
+        }
     }
     if (document.getElementById("runnersLocked").checked != true) {
         const select = document.getElementById("runners");
         const randomIndex = Math.floor(Math.random() * select.options.length);
-        select.value = randomIndex;
+        if (select.value == randomIndex) {
+            select.value = randomIndex ++;
+        }
+        else {
+            select.value = randomIndex;
+        }
     }
     if (document.getElementById("outsLocked").checked != true) {
         const select = document.getElementById("outs");
         const randomIndex = Math.floor(Math.random() * select.options.length);
-        select.value = randomIndex;
+        if (select.value == randomIndex) {
+            select.value = randomIndex ++;
+        }
+        else {
+            select.value = randomIndex;
+        }
     }
 }
